1dc4cfa59265dda25c23723d359efbe11f04a7f4
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-13 14:23:48 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -16,8 +16,6 @@ local uis = game:GetService("UserInputService")
 local GuiLibrary = shared.GuiLibrary
 local ScriptSettings = {}
 local UIS = game:GetService("UserInputService")
-local Lib = GuiLibrary
-local remove = .RemoveObject
 local SMX = function(tab, argstable)
 	return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
 end
@@ -55,4 +53,5 @@ end
 
 infonotify("SWS | SanWare", "has loaded successfully!", 3)
 
-Libremove("XrayOptionsButton")
+GuiLibrary.RemoveObject("XrayOptionsButton")
+GuiLibrary.RemoveObject("SwimOptionsButton")
