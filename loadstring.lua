51ac265f2a6604f4240bb14697690a663e67beaf
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-13 14:46:56 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sail100/RBLXBWSWS/main/script.lua", true))((
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sail100/RBLXBWSWS/main/script.lua", true))()
